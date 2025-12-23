7d7867fcb71408d8277a469d368cf340aaddbd8b
juwel220 <ahmedjuwel2209@gmail.com>
2025-12-24 01:24:41 +0600

    final clean server
    Updated log messages for order submission to be more descriptive.

--- server.js
+++ server.js
@@ -7,17 +7,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// homepage
+// serve homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// order submit
+// handle order
 app.post("/order", (req, res) => {
   const { product, price, number, trx } = req.body;
 
-  console.log("NEW ORDER:");
-  console.log(product, price, number, trx);
+  console.log("NEW ORDER RECEIVED:");
+  console.log("Product:", product);
+  console.log("Price:", price);
+  console.log("Mobile:", number);
+  console.log("Transaction ID:", trx);
 
   res.json({ success: true });
 });
